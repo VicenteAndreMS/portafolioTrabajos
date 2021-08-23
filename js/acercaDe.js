81e357acc1e222f2d4873e99396c16234f3dddf7
VicenteMineroDeveloper <89354692+VicenteMineroDeveloper@users.noreply.github.com>
2021-08-23 18:50:15 -0500

    Delete acercaDe.js

--- js/acercaDe.js
+++
@@ -1,8 +0,0 @@
-function darkMobile(){
-const dark = document.querySelector('#boton-acercaDeCambio');
-const acercaDe = document.querySelector('.acercaDe');
-
-dark.addEventListener('touchend', e =>{
-    acercaDe.classList.toggle('.acercaDeModoOscuro');
-})
-}
