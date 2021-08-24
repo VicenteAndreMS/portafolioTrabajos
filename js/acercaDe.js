50a2c725149f2c3bc186e899aa66b2cc5e41ffe4
VicenteMineroDeveloper <89354692+VicenteMineroDeveloper@users.noreply.github.com>
2021-08-23 20:20:00 -0500

    Add files via upload

---
+++ js/acercaDe.js
@@ -0,0 +1,9 @@
+const dark = document.querySelector('#boton-acercaDeCambio');
+const acercaDe = document.querySelector('.acercaDe');
+
+
+dark.addEventListener('click', e =>{
+    acercaDe.classList.toggle('acercaDeModoOscuro');
+})
+
+
